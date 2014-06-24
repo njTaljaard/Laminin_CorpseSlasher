cc83a6311d5bddc7b3823526c4761eb2eaa55571
Schoeman-301-u10651994 <u10651994@tuks.co.za>
2014-06-24 15:38:52 +0200

    Added a required folder and changed database
    Deleted unnecessary table columns in user table:
    screenName, dateOfBirth and gender.

---
+++ Required/Server/Database.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 24, 2014 at 03:35 PM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `corpseslasher`
+--
+CREATE DATABASE `corpseslasher` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `corpseslasher`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `name` text NOT NULL,
+  `surname` text NOT NULL,
+  `email` text NOT NULL,
+  `zombieKills` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `name`, `surname`, `email`, `zombieKills`) VALUES
+(5, 'u10651994', '1234', 'Martin', 'Schoeman', 'firstnamemartins@yahoo.com', 0);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
