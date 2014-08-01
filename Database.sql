65d4871b3f5d713d4e3749edac5d0b3f1a08df10
Schoeman-301-u10651994 <u10651994@tuks.co.za>
2014-08-01 09:44:45 +0200

    Update database.
    Removed not used fields

--- Database.sql
+++ Database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 02, 2014 at 05:20 PM
+-- Generation Time: Aug 01, 2014 at 09:43 AM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -32,23 +32,19 @@ CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` text NOT NULL,
   `password` text NOT NULL,
-  `screenName` text NOT NULL,
   `name` text NOT NULL,
   `surname` text NOT NULL,
-  `dateOfBirth` date NOT NULL,
-  `gender` tinyint(1) NOT NULL,
   `email` text NOT NULL,
   `zombieKills` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `screenName`, `name`, `surname`, `dateOfBirth`, `gender`, `email`, `zombieKills`) VALUES
-(2, '10651994', '1232', 'MartinS', 'Martin', 'Schoeman', '1990-06-19', 1, 'm@s.com', 0),
-(3, 'usern', 'pass', 'sn', 'n', 's', '2000-05-20', 1, 'm2g.com', 0);
+INSERT INTO `user` (`id`, `username`, `password`, `name`, `surname`, `email`, `zombieKills`) VALUES
+(5, 'u10651994', '1234', 'Martin', 'Schoeman', 'firstnamemartins@yahoo.com', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
