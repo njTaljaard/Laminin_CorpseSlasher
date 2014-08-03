87fe95c8bfb4ea3632dde555f8e8beaf13ce1b65
Nico Taljaard <taljaard.nj@gmail.com>
2014-08-03 16:20:43 +0200

    Disabled collision debugger.

--- CorpseSlasher/src/CorpseSlasher/GameScene.java
+++ CorpseSlasher/src/CorpseSlasher/GameScene.java
@@ -38,7 +38,6 @@ public class GameScene {
         initScene(assestManager, viewPort, cam, bullet, selectedMap, ui, settings);
         initMainCharacter(assestManager, inMan, bullet, cam);
         initMobs(bullet, assestManager);
-        bullet.getPhysicsSpace().enableDebug(assestManager);
 
     }
     
