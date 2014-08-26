fc432c938197deb0e7fad4b8d236c31884f3774a
GerhardSmit <gsmit16@gmail.com>
2014-08-26 20:36:00 +0200

    Files generated for mobile project

---
+++ CorpseSlasherAndroid/mobile/gen/R.java.d
@@ -0,0 +1,4 @@
+C:\Users\Gerhard\Desktop\Laminin_CorpseSlasher\CorpseSlasherAndroid\mobile\gen\com\lamnin\corpseslasher\R.java \
+ : C:\Users\Gerhard\Desktop\Laminin_CorpseSlasher\CorpseSlasherAndroid\mobile\res\layout\main.xml \
+C:\Users\Gerhard\Desktop\Laminin_CorpseSlasher\CorpseSlasherAndroid\mobile\res\values\strings.xml \
+C:\Users\Gerhard\Desktop\Laminin_CorpseSlasher\CorpseSlasherAndroid\mobile\bin\AndroidManifest.xml \
