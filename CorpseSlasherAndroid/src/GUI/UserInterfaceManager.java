e51e02e2029c2360dd6bb96119fc72b983ef34c9
GerhardSmit <gsmit16@gmail.com>
2014-08-26 20:05:08 +0200

    GUIs work for full HD phones

--- CorpseSlasherAndroid/src/GUI/UserInterfaceManager.java
+++ CorpseSlasherAndroid/src/GUI/UserInterfaceManager.java
@@ -75,7 +75,7 @@ public final class UserInterfaceManager {
         guiScreens[0] = new LoginScreen(assetManager, inputManager, audioRenderer, guiViewPort, appState, app,
                                 Screen);
         guiScreens[0].build();
-        guiScreens[0].updateRes(1366, 768);
+        guiScreens[0].updateRes(1920, 1080);
     }
     /**
      * Creates the new account account screen
@@ -84,7 +84,7 @@ public final class UserInterfaceManager {
          guiScreens[1] = new NewAccount(assetManager, inputManager, audioRenderer, guiViewPort, appState, app,
                                            Screen);
         guiScreens[1].build();
-        guiScreens[1].updateRes(1366, 768);
+        guiScreens[0].updateRes(1920, 1080);
     }
     /**
      * Creates the retrieve password screen
@@ -93,12 +93,12 @@ public final class UserInterfaceManager {
         guiScreens[2] = new RetrievePassword(assetManager, inputManager, audioRenderer, guiViewPort,
                                         appState, app, Screen);
         guiScreens[2].build();
-        guiScreens[2].updateRes(1366, 768);
+        guiScreens[0].updateRes(1920, 1080);
     }
     public void leaderBoard() {
         guiScreens[5] = new Leaderboard(assetManager, inputManager, audioRenderer, guiViewPort, appState, app, Screen,client);
         guiScreens[5].build();
-       guiScreens[5].updateRes(width, height);
+        guiScreens[0].updateRes(1920, 1080);
     }
     /**
      * Updates the state of the game so that settings menu can be called after login only
@@ -126,7 +126,7 @@ public final class UserInterfaceManager {
                         inputManager.setCursorVisible(true);
                         guiScreens[3].goTo("Option_Screen");  
                         //state.setEnabled(false);
-                        guiScreens[3].updateRes(width, height);
+        guiScreens[0].updateRes(1920, 1080);
                     } else {
                        // state.setEnabled(true);
                         guiViewPort.getProcessors().removeAll(guiViewPort.getProcessors());
@@ -151,7 +151,8 @@ public final class UserInterfaceManager {
      */
     public void loadingScreen() {
         guiScreens[4] = new LoadingScreen(assetManager, inputManager, audioRenderer, guiViewPort, appState, app, Screen);
-        guiScreens[4].updateRes(width, height);
+
+        guiScreens[0].updateRes(1920, 1080);
     }
     /**
      * 
